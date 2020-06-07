ebafc374ccfd0101cf41fe2b564f0336fc20aaec
valorad <valorad@outlook.com>
2020-06-07 14:41:46 -0400

    player graph add: prevent duplicate dbname

--- App/Controllers/Graphs/PlayerGraph.cs
+++ App/Controllers/Graphs/PlayerGraph.cs
@@ -24,6 +24,17 @@ namespace App.Controllers.Graphs
     [GraphQLMetadata("addPlayer")]
     public async Task<CUDMessage> AddPlayer(Player newPlayer)
     {
+
+      // Check if has existing player
+      Player playerInDB = await playerService.Get(newPlayer.DBName);
+      if (playerInDB is {}) {
+        return new CUDMessage() {
+          OK = false,
+          NumAffected = 0,
+          Message = $"Forbidden to add {newPlayer.DBName} becasue same dbname already Exists",
+        };
+      }
+
       if (newPlayer.Games is null) {
         newPlayer.Games = new List<string>() {};
       }
