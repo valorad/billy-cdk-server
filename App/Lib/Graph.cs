5825270f5a7c07089d024059173679c1d33c3d08
valorad <valorad@outlook.com>
2020-06-07 14:42:39 -0400

    load Graph schema definitions from files

---
+++ App/Lib/Graph.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace App.Lib
+{
+  public static class Graph
+  {
+    public static string LoadDefinitions()
+    {
+      string filePath = Path.GetFullPath(@"Models\Graphs\");
+      var extensions = new List<string>() { ".graphql", ".gql" };
+
+      List<string> files = Directory.GetFiles(filePath)
+        .Where(file => extensions.Any(file.ToLower().EndsWith)).ToList();
+
+      string typeDefs = "";
+      // read text from each file and combine
+      foreach (var file in files)
+      {
+        typeDefs += File.ReadAllText(file);
+      }
+      return typeDefs;
+    }
+  }
+}
--- App/Startup.cs
+++ App/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,113 +18,89 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using App.Lib;
 
 namespace App
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            // add secrets
-            services.Configure<DBConfig>(
-              Configuration.GetSection("mongo")
-            );
-
-            services.AddSingleton<IDBConfig>(sp =>
-              sp.GetRequiredService<IOptions<DBConfig>>().Value
-            );
-
-            // configure db
-            services.AddTransient<IDBContext, DBAccess>();
-            services.AddTransient<IDBCollection, DBCollection>();
-
-            // add services
-            services.AddSingleton<IPlayerService, PlayerService>();
-            services.AddSingleton<IGameService, GameService>();
-            services.AddSingleton<ICDKService, CDKService>();
-
-            // add GraphQL
-            // services.AddSingleton<RootGraph>();
-            services.AddSingleton<Query>();
-            services.AddSingleton<Mutation>();
-            services.AddSingleton<ISchema>(provider => Schema.For(@"
-                type Player {
-                    _id: ID
-                    dbname: String
-                    isPremium: Boolean
-                    games: [String]
-                }
-
-                input PlayerView {
-                    dbname: String!
-                    isPremium: Boolean!
-                    games: [String]
-                }
-
-                type CUDMessage {
-                    ok: Boolean
-                    numAffected: Int
-                    message: String
-                }
-
-                type Query {
-                    player(dbname: String!): Player
-                }
-
-                type Mutation {
-                    addPlayer(newPlayer: PlayerView!): CUDMessage
-                }
-            ", _ =>
-            {
-                _.Types.Include<Query>();
-                _.Types.Include<Mutation>();
-                _.ServiceProvider = provider;
-            }));
-
-            services
-                .AddGraphQL((services, options) =>
-                {
-                    options.EnableMetrics = true;
-                    options.ExposeExceptions = true;
-                    var logger = services.GetRequiredService<ILogger<Startup>>();
-                    options.UnhandledExceptionDelegate = ctx => logger.LogError("{Error} occured", ctx.OriginalException.Message);
-                })
-            .AddSystemTextJson(deserializerSettings => { }, serializerSettings => { })
-            .AddDataLoader();            
-
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-            // use graphQL
-            app.UseGraphQL<ISchema>("/gql");
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      // add secrets
+      services.Configure<DBConfig>(
+        Configuration.GetSection("mongo")
+      );
+
+      services.AddSingleton<IDBConfig>(sp =>
+        sp.GetRequiredService<IOptions<DBConfig>>().Value
+      );
+
+      // configure db
+      services.AddTransient<IDBContext, DBAccess>();
+      services.AddTransient<IDBCollection, DBCollection>();
+
+      // add services
+      services.AddSingleton<IPlayerService, PlayerService>();
+      services.AddSingleton<IGameService, GameService>();
+      services.AddSingleton<ICDKService, CDKService>();
+
+      // add GraphQL
+      services.AddSingleton<Query>();
+      services.AddSingleton<Mutation>();
+      services.AddSingleton<ISchema>(
+        (provider) => Schema.For(Graph.LoadDefinitions(), _ =>
+          {
+            _.Types.Include<Query>();
+            _.Types.Include<Mutation>();
+            _.ServiceProvider = provider;
+          }
+        )
+      );
+
+      services
+          .AddGraphQL((services, options) =>
+          {
+            options.EnableMetrics = false;
+            options.ExposeExceptions = true;
+            var logger = services.GetRequiredService<ILogger<Startup>>();
+            options.UnhandledExceptionDelegate = ctx => logger.LogError("{Error} occured", ctx.OriginalException.Message);
+          })
+      .AddSystemTextJson(deserializerSettings => { }, serializerSettings => { })
+      .AddDataLoader();
+
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+
+      // use graphQL
+      app.UseGraphQL<ISchema>("/gql");
+    }
+  }
 }
