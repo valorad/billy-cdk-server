849806ca3ddad155d1ef05d79017961101981c89
valorad <valorad@outlook.com>
2020-06-07 17:20:52 -0400

    graph mutation: add multiple players

--- App/Controllers/Graphs/PlayerGraph.cs
+++ App/Controllers/Graphs/PlayerGraph.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using App.Models;
 using GraphQL;
@@ -26,12 +28,17 @@ namespace App.Controllers.Graphs
     {
 
       // Check if has existing player
-      Player playerInDB = await playerService.Get(newPlayer.DBName);
+      Player playerInDB = await playerService.Get(
+        newPlayer.DBName,
+        new DBViewOption() {
+          Includes = new List<string>() {"dbname"}
+        }
+      );
       if (playerInDB is {}) {
         return new CUDMessage() {
           OK = false,
           NumAffected = 0,
-          Message = $"Forbidden to add {newPlayer.DBName} becasue same dbname already Exists",
+          Message = $"Forbidden to add {newPlayer.DBName} becasue the same dbname already exists",
         };
       }
 
@@ -48,6 +55,66 @@ namespace App.Controllers.Graphs
       }
       return message; 
     }
+
+    [GraphQLMetadata("addPlayers")]
+    public async Task<CUDMessage> AddPlayer(List<Player> newPlayers)
+    {
+
+      // stop here if list is empty
+      if (newPlayers.Count <= 0) {
+        return new CUDMessage() {
+          OK = false,
+          NumAffected = 0,
+          Message = $"No players are added since the add list is empty.",
+        };
+      }
+
+      // remove conflict players in the add list
+      var dbnameQuoted = newPlayers.Select((ele) => $"\"{ele.DBName}\"" );
+      string dbnameInText = string.Join(", ", dbnameQuoted);
+      
+      string conditions = "{"
+      + " \"dbname\": " + "{"
+        + "\"$in\": [" + dbnameInText + "]"
+        + "}"
+      + "}";
+      List<Player> conflictPlayers = await playerService.Get(
+        JsonDocument.Parse(conditions).RootElement,
+        new DBViewOption() {
+          Includes = new List<string>() {"dbname"}
+        }
+      );
+
+      foreach (var player in conflictPlayers) {
+        newPlayers.Remove(newPlayers.Find(ele => ele.DBName == player.DBName));
+      }
+
+      if (newPlayers.Count <= 0) {
+        return new CUDMessage() {
+          OK = false,
+          NumAffected = 0,
+          Message = $"Forbidden to add these players becasue they all have conflicted dbname-s with existing players.",
+        };
+      }
+
+      // polyfill game field
+      foreach (var player in newPlayers) {
+        if (player.Games is null) {
+          player.Games = new List<string>() {};
+        }
+      }
+
+      CUDMessage message = await playerService.Add(newPlayers);
+      if (!message.OK) {
+        Console.WriteLine(message.Message);
+        message.Message = $"Failed to add {newPlayers.Count} players. See logs for more details.";
+      } else {
+        message.Message = $"Successfully added {newPlayers.Count} players.";
+      }
+      return message; 
+    }
+
   }
+
 
 }
---
+++ App/Lib/Serializer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Buffers;
+using System.Text.Json;
+
+namespace App.Lib
+{
+  public static class Serializer {
+    public static T JSONToObject<T>(this JsonElement element, JsonSerializerOptions options = null)
+    {
+        var bufferWriter = new ArrayBufferWriter<byte>();
+        using (var writer = new Utf8JsonWriter(bufferWriter))
+            element.WriteTo(writer);
+        return JsonSerializer.Deserialize<T>(bufferWriter.WrittenSpan, options);
+    }
+
+    public static T JSONToObject<T>(this JsonDocument document, JsonSerializerOptions options = null)
+    {
+        if (document == null)
+            throw new ArgumentNullException(nameof(document));
+        return document.RootElement.JSONToObject<T>(options);
+    }
+  }
+}
